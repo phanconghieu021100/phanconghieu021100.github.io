426615b9117488e0aa3f2f2182c83be76c9faf3e
hieupc <phanconghieu321@gmail.com>
2025-07-04 11:26:34 +0700

    update

--- bash deploy_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-flutter build web
-cd build/web
-git init
-git remote add origin https://github.com/phanconghieu021100/phanconghieu021100.github.io.git
-git checkout -b main
-git add .
-git commit -m "deploy web"
-git push -f origin main
